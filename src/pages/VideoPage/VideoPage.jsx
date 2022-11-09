f7418c090722e1ec3cabae934baa581a4fdf6bfa
jaypostle <50502972+jaypostle@users.noreply.github.com>
2022-11-08 18:15:59 -0800

    fetch data inital part

--- src/pages/VideoPage/VideoPage.jsx
+++ src/pages/VideoPage/VideoPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Hero from '../../components/sections/Hero/Hero';
 import MovieDetails from '../../components/sections/MovieDetails/MovieDetails';
 import CommentWall from '../../components/sections/CommentWall/CommentWall';
@@ -6,13 +6,16 @@ import NextVideos from '../../components/sections/NextVideos/NextVideos';
 // import VideoDetails from './data/video-details.json';
 import { useParams } from 'react-router-dom';
 import {getVideos, getVideoDetails} from '../../utilities/utilities';
+import axios from 'axios';
 
 import '../../App.scss';
 
 function VideoPage() {
-  const { paramsVideoId } = useParams();
-  console.log(paramsVideoId);
+//   const { paramsVideoId } = useParams();
+//   console.log(paramsVideoId);
 
+
+// **** OLD WAY ///
   const [currentVideoId, setCurrentVideoId] = useState("84e96018-4022-434e-80bf-000ce4cd12b8"); // takes an id
 
   // This is for the side panel
@@ -30,7 +33,28 @@ function VideoPage() {
     setVideos(getVideos(videoId));
     setVideoExpandedDetails(getVideoDetails(videoId));
   }
+
+
+  // **** FETCH USE EFFECT **** ///
+  const SEARCH_URL = `https://project-2-api.herokuapp.com/videos`;
+  const API_KEY = `?api_key=${process.env.REACT_APP_BRAINFLIX_API_KEY}`;
+  const searchByVideoId = (videoId) => `${SEARCH_URL}/${videoId}${API_KEY}`;
   
+
+
+  // * Fetch Total Video Data * //
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const { data } = await axios.get(`${SEARCH_URL}${API_KEY}`)
+            console.log(data);
+            setVideos(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    fetchData();
+  }, [])
 
   return (
     <div className="App">
